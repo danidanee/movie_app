e7728b8c499045c7b2292d0798318039b0bbeb2a
danidanee <ssongda9412@naver.com>
2020-01-06 22:25:10 +0900

    Food

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+
+const foodILike = [
+  {
+    id:1,
+    name : "kimchi",
+    image:"http://image.auction.co.kr/itemimage/19/b9/2f/19b92f7766.jpg",
+    rating : 5
+  },
+  {
+    id:2,
+    name : "KimBap",
+    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F60ANBnHjiDU%2Fmaxresdefault.jpg&f=1&nofb=1",
+    rating : 4.7
+  }
+]
+
+function Food({name, picture, rating}){
+  return(
+    <div>
+      <h1>I Love {name}</h1>
+      <h4>{rating}/5.0</h4>
+      <img src={picture}></img>
+    </div>
+  );
+}
+
+// 타입, 변수가 맞는 지 확인하는 함수
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foodILike.map(a => (
+        <Food key={a.id} name={a.name} picture={a.image} rating={a.rating}/>
+      ))}
     </div>
   );
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+
+
+// ReactDOM : 라이브러리를 웹사이트에 출력해둠, 한 개의 컴포넌트를 출력(render)
+// document 안에 element가 있는데, element ID('root')는 인덱스 html 파일에 있음
+// application은 한번에 하나의 component만 출력할 수 있다
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
