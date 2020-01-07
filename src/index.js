82d3bd17a8fa60f7eddcc4a8f1094cd99e319d7e
danidanee <ssongda9412@naver.com>
2020-01-07 12:15:56 +0900

    deploy to git

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
-
 // ReactDOM : 라이브러리를 웹사이트에 출력해둠, 한 개의 컴포넌트를 출력(render)
 // document 안에 element가 있는데, element ID('root')는 인덱스 html 파일에 있음
 // application은 한번에 하나의 component만 출력할 수 있다
