261991bccd41f2fef87782556c53e47d0ace6175
danidanee <ssongda9412@naver.com>
2020-01-07 11:41:56 +0900

    Updates

---
+++ precache-manifest.ac58bd60238fe0477bf22007e2e2e6cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9af1158380fbd070d044fa59c22a57f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "16697246edb9b4452fcd",
+    "url": "/movie_app/static/css/main.5e0048cf.chunk.css"
+  },
+  {
+    "revision": "02c803fac3999cf1665c",
+    "url": "/movie_app/static/js/2.2b9d6ecc.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app/static/js/2.2b9d6ecc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16697246edb9b4452fcd",
+    "url": "/movie_app/static/js/main.dcf39e32.chunk.js"
+  },
+  {
+    "revision": "b03db121bc0725d553bd",
+    "url": "/movie_app/static/js/runtime-main.4970d62d.js"
+  }
+]);
