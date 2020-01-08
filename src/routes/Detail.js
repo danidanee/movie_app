51e28ab96d0c5ee0cf2d84b36aa2007db1ad2c50
danidanee <ssongda9412@naver.com>
2020-01-08 11:04:42 +0900

    lecture finish

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-function Detail(props){
-    return (
-    <span>hello</span>
-    );
+class Detail extends React.Component {
+    
+    // 제일 먼저 실행되는 라이프 사이클
+    componentDidMount(){
+        const { location, history } = this.props;
+
+        //만약 입력해서 movie_detail로 가면 홈으로 보낸다
+        if(location.state === undefined){
+            history.push("/");
+        }
+    }
+
+    // 그런데 render가 먼저 실행된다
+    render() {
+        const { location } = this.props;
+        if (location.state) {
+          return <span>{location.state.title}</span>;
+        } else {
+          return null;
+        }
+    }
 }
 
 export default Detail;
